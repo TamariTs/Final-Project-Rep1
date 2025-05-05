5e2ae0b07cec613f3e9a29dfccf96f03d8975640
TamariTs <takotsikara@gmail.com>
2025-05-05 23:50:32 +0400

    save

--- cypress/support/myCommandFinal.js
+++ cypress/support/myCommandFinal.js
@@ -9,7 +9,24 @@ Cypress.Commands.add("registrationFinalP", (Userdatta)=>{
     cy.get(':nth-child(5) > .ipass').type(Userdatta.password)
     cy.get('.reg-form-left > :nth-child(6) > .ipass').type(Userdatta.confirmPassword)
     cy.get('#Path_10302').click()
-    //cy.get('.regsub').click()
+    cy.get('.regsub').click()
+}
+
+)
+Cypress.Commands.add("incorrectRegistration", (Userdata)=>{
+    cy.get('.menu-pop > .rprof').click()
+    cy.get('.input-shablon > p > a').should("be.visible")
+    cy.get('.input-shablon > p > a').click()
+    cy.get(':nth-child(1) > .ismile').type(Userdata.name)
+    cy.get(':nth-child(2) > .imail').type(Userdata.email).should("have.value", Userdata.email)
+    cy.get('.ipir').type(Userdata.ID)
+    cy.get(':nth-child(4) > .itel').type(Userdata.phone)
+    cy.get(':nth-child(5) > .ipass').type(Userdata.password)
+    cy.get('.reg-form-left > :nth-child(6) > .ipass').type(Userdata.confirmPassword)
+    cy.get('#Path_10302').click()
+    cy.get('.regsub').click()
+    cy.get('.input-div.alert > .alert > img').should("be.visible") 
+    cy.get('.rprof > p').should("have.text", "შესვლა")
 }
 
 )
@@ -22,3 +39,14 @@ cy.get('.menu-pop > .rprof').click()
      cy.get('.menu-pop > .iprof > p').click().should("have.text", "პროფილი")}
 
     )
+
+    Cypress.Commands.add("Incorrectauthorization", (Userdatta)=>{
+        cy.get('.menu-pop > .rprof').click()
+             cy.get(':nth-child(5) > .imail').type(Userdatta.email).should("have.value", Userdatta.email)
+             cy.get('.ipass').type(Userdatta.password)
+             cy.get('.avtorization > .input-shablon > .form-button').click()
+             cy.get('.input-div.alert > .alert > img').should("be.visible")
+             cy.get('.avtorization > .input-shablon > .form-button').should("have.text", "ავტორიზაცია")}
+             
+        
+            )
